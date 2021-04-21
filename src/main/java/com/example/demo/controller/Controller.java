753d00bfa3b18ebaa4fdf27dc1cd0fa7a8eed458
Roman <richy.corp@gmail.com>
2021-04-22 03:12:11 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping(value = "/check",produces= MediaType.TEXT_PLAIN_VALUE,consumes = MediaType.TEXT_PLAIN_VALUE)
+    public String getHello(@RequestBody String val){
+        return "hello " +  val;
+    }
+}
